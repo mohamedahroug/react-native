813b3b6526ca6ad8ae37d3321d6a2fb0e1ca03b3
mohameddahroug <mhmeddahroug@gmail.com>
2025-06-28 17:25:43 +0300

    task done

--- App.js
+++ App.js
@@ -1,20 +1,95 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
+
 
 export default function App() {
+  const films = [
+    {
+      id:1,
+      name:'Lego Star Wars Terrifying Talk',
+      img: require('./assets/image1.png'),
+    },
+    {
+      id:2,
+      name:'Lego Star Wars Terrifying Talk',
+      img: require('./assets/image2.png'), 
+    },
+    {
+      id:3,
+      name:'Lego Star Wars Terrifying Talk',
+      img: require('./assets/image3.png'),
+    },
+    {
+      id:4,
+      name:'Lego Star Wars Terrifying Talk',
+      img: require('./assets/image4.png'),
+    },
+  ]
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView  contentContainerStyle={styles.container}>
+       <StatusBar style='dark'/>
+      <Image source={require('./assets/logonew.png')} style={{ width: 200, height: 200 ,resizeMode:'contain',alignSelf:'center'}} />
+      <Image source={require("./assets/hero.png")} style={{width:400 ,resizeMode:'contain'}} />
+      <View >
+        <Text style={{fontSize:20,fontWeight:'bold',marginTop:10}} >Brand</Text>
+        <ScrollView horizontal={true} contentContainerStyle={styles.brand} >
+          <Image source={require("./assets/logo1.png")} style={styles.logo}/>
+          <Image source={require("./assets/logo2.png")} style={styles.logo}/>
+          <Image source={require("./assets/logo3.png")} style={styles.logo}/>
+          <Image source={require("./assets/logo1.png")} style={styles.logo}/>
+          <Image source={require("./assets/logoo.png")} style={styles.logo}/>
+          <Image source={require("./assets/logo2.png")} style={styles.logo}/>
+        </ScrollView>
+      </View>
+      <View >
+        <Image source={require("./assets/logonew.png")} style={{alignItems:'start'}} />
+      </View>
+    <ScrollView>
+
+
+      {
+        films.map((film)=>(
+          <View key={film.id} style={styles.film}>
+            <Image source={film.img} style={styles.image} />
+            <Text style={{fontSize:16}} >{film.name}</Text>
+          </View>
+        ))
+      }
+      </ScrollView>
+      
+
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'lightblue',
+    // alignItems: 'center',
+    padding:10,
+    // justifyContent: 'center',
+   
   },
+  brand:{
+    marginTop:10,
+    height:50,
+    alignItems:'center', 
+    marginBottom:30,
+  },
+  logo:{
+  
+    // width:80,
+    marginRight:15,
+  },
+  film:{
+    backgroundColor:'#fff',
+    borderRadius:10,
+    padding:10,
+    margin:10,
+  },
+  image:{
+    width:'100%', 
+    borderRadius:10
+  }
 });
